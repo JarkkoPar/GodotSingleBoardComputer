61a6caa623f1a45b0c8da6d04189eb3f687782e3
Jarkko Parviainen <jarkko.parviainen80@gmail.com>
2023-08-05 11:16:21 +0200

    Added basis for the I2cSda1115.

---
+++ I2cSda1115.cpp
@@ -0,0 +1,37 @@
+#include "I2cSda1115.h"
+
+
+using namespace godot;
+
+I2cSda1115::I2cSda1115() {
+
+}
+
+I2cSda1115::~I2cSda1115() {
+    
+}
+
+
+void I2cSda1115::_bind_methods() {
+    
+}
+
+
+// Godot virtuals.
+void I2cSda1115::_process(double delta) {
+
+}
+
+void I2cSda1115::_physics_process(double delta) {
+
+}
+
+void I2cSda1115::_notification(int p_what) {
+
+}
+    
+    // Device handling.
+void I2cSda1115::_initialize_device() {
+
+} 
+
