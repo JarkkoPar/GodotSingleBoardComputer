ba83eb4a2e5e40308db5e81a8cc27d5fc2639717
Jarkko Parviainen <jarkko.parviainen80@gmail.com>
2023-08-05 19:03:21 +0200

    Fixed Sda to ADS :P

---
+++ I2cAds1115.cpp
@@ -0,0 +1,37 @@
+#include "I2cAds1115.h"
+
+
+using namespace godot;
+
+I2cAds1115::I2cAds1115() {
+
+}
+
+I2cAds1115::~I2cAds1115() {
+    
+}
+
+
+void I2cAds1115::_bind_methods() {
+    
+}
+
+
+// Godot virtuals.
+void I2cAds1115::_process(double delta) {
+
+}
+
+void I2cAds1115::_physics_process(double delta) {
+
+}
+
+void I2cAds1115::_notification(int p_what) {
+
+}
+    
+    // Device handling.
+void I2cAds1115::_initialize_device() {
+
+} 
+
---
+++ I2cAds1115.h
@@ -0,0 +1,82 @@
+#ifndef I2C_SDA1115_H_INCLUDED
+#define I2C_SDA1115_H_INCLUDED
+
+//#include <core/templates/vset.h>
+#include "I2cDevice.h"
+
+namespace godot {
+
+// This is a control node for the PCA9685 servo driver.
+class I2cAds1115 : public I2cDevice {
+    GDCLASS(I2cAds1115, I2cDevice)
+
+private:
+    int _pwm_frequency_hz; 
+    int _pwm_prescale_value;
+    double _pwm_oscillator_frequency;
+    bool _is_ads1115_initialized;
+    double _ads1115_update_frame_delay;
+    double _ads1115_update_wait_time;
+
+    
+protected: 
+    static void _bind_methods();
+
+public:
+
+    enum ADS1115Registers {
+        CONVERSION = 0x00,
+        CONFIGURATION = 0x01,
+        TRESHOLD_LOW = 0x02,
+        TRESHOLD_HIGH = 0x03
+    };
+
+    enum ADS1115Mode {
+        CONTINUOUS = 0x00,
+        SINGLE_SHOT = 0x80
+    };
+
+    enum ADS1115DataRate {
+        SPS_8 = 0x00,
+        SPS_16 = 0x80,
+        SPS_32,
+        SPS_64,
+        SPS_128,
+        SPS_250,
+        SPS_475,
+        SPS_860
+    };
+
+    I2cAds1115();
+    ~I2cAds1115();
+
+     // Godot virtuals.
+    void _process(double delta);
+    void _physics_process(double delta);
+
+    void _notification(int p_what);
+
+    
+    // Getters and setters.
+    //void set_pwm_frequency_hz( int new_frequency_hz );
+    //int  get_pwm_frequency_hz() const;    
+
+    
+    // Device handling.
+    virtual void _initialize_device() override;
+    
+    //void wake_ads1115();
+    //void sleep_ads1115();
+    //void restart_ads1115();
+
+
+};
+
+
+
+
+}
+
+
+
+#endif 
