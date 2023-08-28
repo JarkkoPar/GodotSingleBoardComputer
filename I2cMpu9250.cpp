4b25452f7bbda0bf31faa744451d1518ed573d6d
Jarkko Parviainen <jarkko.parviainen80@gmail.com>
2023-08-28 22:05:35 +0200

    Edited configs.

--- I2cMpu9250.cpp
+++ I2cMpu9250.cpp
@@ -137,21 +137,22 @@ float I2cMpu9250::get_temperature_celsius() const {
 
 bool I2cMpu9250::_configure_i2c_device() {
     // Start by resetting the device and waking it up.
-    uint8_t reset[] = {MPU9250PowerManagement1::POWER_MANAGEMENT_1_RESET};
-    _write_bytes_to_device(_i2c_device_address, MPU9250Registers::PWR_MGMT_1, 1, reset );
+    //uint8_t reset[] = {MPU9250PowerManagement1::POWER_MANAGEMENT_1_RESET};
+    //_write_bytes_to_device(_i2c_device_address, MPU9250Registers::PWR_MGMT_1, 1, reset );
+    _write_byte_to_device(_i2c_device_address, MPU9250Registers::PWR_MGMT_1, MPU9250PowerManagement1::POWER_MANAGEMENT_1_RESET );
     OS::get_singleton()->delay_msec(100);
-    reset[0] = MPU9250PowerManagement1::POWER_MANAGEMENT_1_WAKE;
-    _write_bytes_to_device(_i2c_device_address, MPU9250Registers::PWR_MGMT_1, 1, reset );
-        
+    //reset[0] = MPU9250PowerManagement1::POWER_MANAGEMENT_1_WAKE;
+    //_write_bytes_to_device(_i2c_device_address, MPU9250Registers::PWR_MGMT_1, 1, reset );
+    _write_byte_to_device(_i2c_device_address, MPU9250Registers::PWR_MGMT_1, MPU9250PowerManagement1::POWER_MANAGEMENT_1_WAKE );  
 
     // Configure the Magnetometer, gyroscope and finally the accelerometer.
     uint8_t accel_config[] = {0x18};
     _write_bytes_to_device(_i2c_device_address, MPU9250Registers::ACCEL_CONFIG, 1, accel_config );
-    _write_byte_array_to_device_register(MPU9250Registers::ACCEL_CONFIG, accel_config);
+    _write_bytes_to_device(_i2c_device_address, MPU9250Registers::ACCEL_CONFIG, 1, accel_config);
     uint8_t gyro_config[] = {0x18};
-    _write_byte_array_to_device_register(MPU9250Registers::GYRO_CONFIG, gyro_config);
+    _write_bytes_to_device( _i2c_device_address, MPU9250Registers::GYRO_CONFIG, 1, gyro_config);
     uint8_t magnetometer_config[] = {0x02};
-    _write_byte_array_to_device_register(MPU9250Registers::INT_PIN_CFG, magnetometer_config);
+    _write_byte_to_device(_i2c_device_address, MPU9250Registers::INT_PIN_CFG, magnetometer_config[0]);
     //write_byte_to_device_register_at_device_address( _magnetometer_i2c_address, , uint8_t value );
     //_write_byte_array_to_device_register(MPU9250Registers::INT_PIN_CFG, magnetometer_config);
     
